eacff401f2669aed7b0229b18aaee1b3b3402c4c
Souza-L-01 <souza.vilela.lucas@gmail.com>
2023-07-14 17:29:47 +0200

    add exercise list/object

--- 3_Operators/operators.py
+++ 3_Operators/operators.py
@@ -28,4 +28,21 @@ print(all_numbers)
 
 print([1,2,3] * 3)
 
--------------------------------------
+------------------------------------
+
+x = object()
+y = object()
+
+x_list = [x] * 10
+y_list = [y] * 10
+big_list = x_list + y_list
+
+print("x_list contains %d objects" % len(x_list))
+print("y_list contains %d objects" % len(y_list))
+print("big_list contains %d objects" % len(big_list))
+
+# testing code
+if x_list.count(x) == 10 and y_list.count(y) == 10:
+    print("Almost there...")
+if big_list.count(x) == 10 and big_list.count(y) == 10:
+    print("Great!")
