c225addbc13c29f65825480d775d94a8d7180fb4
Souza-L-01 <souza.vilela.lucas@gmail.com>
2023-07-15 10:54:38 +0200

    add break and continue

--- 7_Loops/loops.py
+++ 7_Loops/loops.py
@@ -25,3 +25,18 @@ while count < 5:
 
 # "break" and "continue" statements
 # break is used to exit a for loop or a while loop, whereas continue is used to skip the current block, and return to the "for" or "while" statement. 
+# Prints out 0,1,2,3,4
+count = 0
+while True:
+    print
+    count += 1
+    if count >= 5:
+        break
+
+# Prints out only odd numbers - 1,3,5,7,9
+for x in range(10):
+    # Check if x is even
+    if x % 2 == 0:
+        continue
+    print(x)
+
