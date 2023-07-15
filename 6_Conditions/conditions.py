1592d9dab0b8161ef9f4cf7aa64b471a092116a6
Souza-L-01 <souza.vilela.lucas@gmail.com>
2023-07-15 10:37:54 +0200

    add more conditons

--- 6_Conditions/conditions.py
+++ 6_Conditions/conditions.py
@@ -55,3 +55,25 @@ print (not False) # Prints out True
 print ((not False) == (False)) # Prints out False
 # Ruby = puts !(false) # Prints out true
 
+number = 16
+second_number = 0
+first_array = []
+second_array = [1,2,3]
+
+if number > 15:
+	print ("1")
+
+if first_array:
+	print ("2")
+
+if len(second_array) == 2:
+	print ("3")
+
+if len(first_array) + len(second_array) == 5:
+	print("4")
+
+if first_array and first_array[0] == 1:
+	print("5")
+
+if not second_array:
+	print("6")
