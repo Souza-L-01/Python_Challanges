94f6368ff0ab6eb82dd3e4d18e9f0ee4e5f23d25
Souza-L-01 <souza.vilela.lucas@gmail.com>
2023-07-17 19:24:45 +0200

    add init() class

--- 9_Classes and Objects/classes_objets.py
+++ 9_Classes and Objects/classes_objets.py
@@ -34,3 +34,14 @@ class MyClass:
 myobjectx = MyClass()
 
 myobjectx.function()
+
+class NumberHolder:
+
+   def __init__(self, number):
+       self.number = number
+
+   def returnNumber(self):
+       return self.number
+
+var = NumberHolder(7)
+print(var.returnNumber()) #Prints '7'
