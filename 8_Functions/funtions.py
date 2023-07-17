97175d5f0dc31bb1d142b7d43ed64849a92de990
Souza-L-01 <souza.vilela.lucas@gmail.com>
2023-07-17 19:03:43 +0200

    start funtion

--- 8_Functions/funtions.py
+++ 8_Functions/funtions.py
@@ -1,3 +1,9 @@
 # Functions
 # What are Functions?
 # Functions are a convenient way to divide your code into useful blocks, allowing us to order our code, make it more readable, reuse it and save some time. Also functions are a key way to define interfaces so programmers can share their code.
+
+def my_function():
+    print("Hello From My Function!")    
+
+def my_function_with_args(username, greeting):
+    print("Hello, %s, From my function! I wish you %s" % (username, greeting))
