7f58b41fc9f84dbbe52e6a563258e7fa90bc7372
Souza-L-01 <souza.vilela.lucas@gmail.com>
2023-07-17 19:10:25 +0200

    new funtions

--- 8_Functions/funtions.py
+++ 8_Functions/funtions.py
@@ -22,3 +22,18 @@ my_function_with_args("John Doe", "a great year!")
 # after this line x will hold the value 3!
 x = sum_two_numbers(1,2)
     print(x)
+    
+# Modify this function to return a list of strings as defined above
+def list_benefits():
+    return []
+
+# Modify this function to concatenate to each benefit - " is a benefit of functions!"
+def build_sentence(benefit):
+    return ""
+
+def name_the_benefits_of_functions():
+    list_of_benefits = list_benefits()
+    for benefit in list_of_benefits:
+        print(build_sentence(benefit))
+
+name_the_benefits_of_functions()
