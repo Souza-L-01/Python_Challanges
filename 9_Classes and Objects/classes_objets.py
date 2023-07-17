8d74b9e3d549deef50751c27d6f7ca7a400f8d99
Souza-L-01 <souza.vilela.lucas@gmail.com>
2023-07-17 19:23:21 +0200

    print variables

--- 9_Classes and Objects/classes_objets.py
+++ 9_Classes and Objects/classes_objets.py
@@ -10,3 +10,17 @@ myobjectx = MyClass()
 
 myobjectx.varible
 
+class MyClass:
+    variable = "blah"
+
+    def function(self):
+        print("This is a message inside the class.")
+
+myobjectx = MyClass()
+myobjecty = MyClass()
+
+myobjecty.variable = "yackity"
+
+# Then print out both values
+print(myobjectx.variable)
+print(myobjecty.variable)
