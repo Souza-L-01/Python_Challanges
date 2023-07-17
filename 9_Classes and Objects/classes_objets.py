ab36e3f5d938bb5deb77c78a99ff46d8c2199881
Souza-L-01 <souza.vilela.lucas@gmail.com>
2023-07-17 19:22:50 +0200

    add exemple class

--- 9_Classes and Objects/classes_objets.py
+++ 9_Classes and Objects/classes_objets.py
@@ -1,3 +1,12 @@
 # Classes and Objects
 # Objects are an encapsulation of variables and functions into a single entity. Objects get their variables and functions from classes. Classes are essentially a template to create your objects.
 
+class MyClass:
+    varible = "blah"
+
+    def function(self):
+        print("This is a message inside the class.")
+myobjectx = MyClass()
+
+myobjectx.varible
+
