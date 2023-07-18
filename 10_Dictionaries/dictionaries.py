93bd0627d3691ff144eba8bcd6b2dfd7d31121ff
Souza-L-01 <souza.vilela.lucas@gmail.com>
2023-07-18 17:52:09 +0200

    start dictionary

---
+++ 10_Dictionaries/dictionaries.py
@@ -0,0 +1,10 @@
+# Dictionaries
+# A dictionary is a data type similar to arrays, but works with keys and values instead of indexes. Each value stored in a dictionary can be accessed using a key, which is any type of object (a string, a number, a list, etc.) instead of using its index to address it.
+# Hash in ruby
+
+phonebook = {
+  "John" : 938477566,
+  "Jack" : 938377264,
+  "Jill" : 947662781
+}
+print(phonebook)
