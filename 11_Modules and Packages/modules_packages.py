237b63233407291dee6f522c5643da2f247d4de3
Souza-L-01 <souza.vilela.lucas@gmail.com>
2023-07-19 16:44:04 +0200

    write packages

--- 11_Modules and Packages/modules_packages.py
+++ 11_Modules and Packages/modules_packages.py
@@ -101,3 +101,17 @@ urllib.urlopen(...)
 
 #  we can read more about it with the help function, using the Python interpreter:
 help(urllib.urlopen)
+
+# Writing packages
+# Packages are namespaces containing multiple packages and modules. They're just directories, but with certain requirements.
+# Each package in Python is a directory which MUST contain a special file called __init__.py. This file, which can be empty, indicates that the directory it's in is a Python package. That way it can be imported the same way as a module.
+# If we create a directory called foo, which marks the package name, we can then create a module inside that package called bar. Then we add the __init__.py file inside the foo directory.
+# To use the module bar, we can import it in two ways:
+
+import foo.bar
+from foo import bar
+
+# The __init__.py file can also decide which modules the package exports as the API, while keeping other modules internal, by overriding the __all__ variable like so:
+__init__.py:
+
+__all__ = ["bar"]
