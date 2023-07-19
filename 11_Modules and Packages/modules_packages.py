774459efb8c2bb642367a397baf575367bb9cebe
Souza-L-01 <souza.vilela.lucas@gmail.com>
2023-07-19 16:18:17 +0200

    study of modules

---
+++ 11_Modules and Packages/modules_packages.py
@@ -0,0 +1,18 @@
+# Writing modules
+# Modules in Python are just Python files with a .py extension. The name of the module is the same as the file name. A Python module can have a set of functions, classes, or variables defined and implemented. 
+
+# game.py
+# import the draw module
+import draw
+
+def play_game():
+	...
+
+def main():
+	result = play_game()
+	draw.draw_game(result)
+
+# this means that if this script is executed, then 
+# main() will be executed
+if __name__ == '__main__':
+	main()
