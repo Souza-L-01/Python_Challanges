6bbec27e104a2e0f040f98d95fae6e6d663ee67c
Souza-L-01 <souza.vilela.lucas@gmail.com>
2023-07-19 16:31:27 +0200

    initialize objects

--- 11_Modules and Packages/modules_packages.py
+++ 11_Modules and Packages/modules_packages.py
@@ -54,3 +54,21 @@ def main():
 	result = play_game()
 	# this can either be visual or textual depending on visual_mode
 	draw.draw_game(result)
+
+# Module initialization
+# The first time a module is loaded into a running Python script, it is initialized by executing the code in the module once. If another module in your code imports the same module again, it will not be loaded again, so local variables inside the module act as a "singleton," meaning they are initialized only once.
+# draw.py
+
+def draw_game():
+	# when clearing the screen we can use the main screen object initialized in this module
+	clear_screen(main_screen)
+	...
+
+def clear_screen(screen):
+	...
+
+class Screen():
+	...
+
+# initialize main_screen as a singleton
+main_screen = Screen()
