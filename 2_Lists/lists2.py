eceef05f7307b25d7bb7b747f2752f94dc5cec3b
Souza-L-01 <souza.vilela.lucas@gmail.com>
2023-08-03 18:36:33 +0200

    list functions

--- 2_Lists/lists2.py
+++ 2_Lists/lists2.py
@@ -1,1 +1,6 @@
 friends = ["Kevin", "Karen", "Jim", "Oscar", "Toby"]
+print(friends)
+print(friends[0])
+print(friends[1])
+print(friends[1:3]) #modify the range of the list
+print(friends[1:]) #print from 1 to the end
