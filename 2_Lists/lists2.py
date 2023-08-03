40325bef942c7b9df6b76c287a79ad057d8b0d3d
Souza-L-01 <souza.vilela.lucas@gmail.com>
2023-08-03 18:40:15 +0200

    array funtions (extend, append, insert)

--- 2_Lists/lists2.py
+++ 2_Lists/lists2.py
@@ -1,5 +1,10 @@
 friends = ["Kevin", "Karen", "Jim", "Oscar", "Toby"]
 lucky_numbers = [4, 8, 15, 16, 23, 42]
+
+friends.extend(lucky_numbers) #add the lucky_numbers list to the friends list
+friends.append("Creed") #add an element to the end of the list
+friends.insert(1, "Kelly") #add an element to the list in a specific position
+
 print(friends)
 print(friends[0])
 print(friends[1])
