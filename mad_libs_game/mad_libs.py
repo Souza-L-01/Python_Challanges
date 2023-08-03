3b2cc43238c0c7993c99355f064700271ef4954e
Souza-L-01 <souza.vilela.lucas@gmail.com>
2023-08-03 18:28:34 +0200

    concatenation to the game

--- mad_libs_game/mad_libs.py
+++ mad_libs_game/mad_libs.py
@@ -1,7 +1,7 @@
 color = input("Enter a color: ")
 plural_noum = input("Enter a plural noum: ")
-celebrity = input("Enter a celebrity: ")
+celebrity = input("Enter a celebrity name: ")
 
-print ("Roses are {color}")
-print ("{plural_noum} are blue")
-print ("I love {celebrity}")
+print ("Roses are " + color)
+print (plural_noum + " are blue")
+print ("I love " + celebrity)
