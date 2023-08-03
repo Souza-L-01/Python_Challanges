8d3a075211a851ad532230d693651eeb43fb705f
Souza-L-01 <souza.vilela.lucas@gmail.com>
2023-08-03 17:35:38 +0200

    print messages

--- 12_values/variable.py
+++ 12_values/variable.py
@@ -1,6 +1,13 @@
 character_name = "Tom"
-character_age = "50"
+character_age = "50" #can only concatenate str (not "int") to str
+is_male = True
 print ("There once was a man named " + character_name + ", ")
 print ("he was " + character_age + " years old. ")
 print ("He really liked the name " + character_name + ", ")
 print ("but he didn't like being " + character_age + " years .")
+
+print ("Giraffe\nAcademy")  # \n is a new line
+print ("Giraffe\"Academy") # \" is a double quote
+
+phrase = "Giraffe Academy"
+print (phrase + " is cool")
