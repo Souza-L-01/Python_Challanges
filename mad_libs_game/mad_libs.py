107dbda6350c94f585ef54ef9d9ed7049a881b14
Souza-L-01 <souza.vilela.lucas@gmail.com>
2023-08-03 18:26:09 +0200

    start mad libs game

---
+++ mad_libs_game/mad_libs.py
@@ -0,0 +1,7 @@
+color = input("Enter a color: ")
+plural_noum = input("Enter a plural noum: ")
+celebrity = input("Enter a celebrity: ")
+
+print ("Roses are {color}")
+print ("{plural_noum} are blue")
+print ("I love {celebrity}")
