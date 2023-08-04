a7d6f33d5449c4fdad8d0ac0901be07cde84d61a
Souza-L-01 <souza.vilela.lucas@gmail.com>
2023-08-04 16:38:46 +0200

    call funtion and parameters

--- 8_Functions/funtion2.py
+++ 8_Functions/funtion2.py
@@ -1,9 +1,11 @@
-def say_hi():
-    print("Hello User") # This is a function, indentation is important
+def say_hi(name, age):
+    print("Hello " + name + " your're " + str(age) ) # This is a function, indentation is important
 
 print("Top")
-say_hi() # This is how you call a function
+# say_hi() # This is how you call a function
 print("Bottom")
 # Top
 # Hello User
 # Bottom
+
+say_hi("Mike", 35)
