39e207fa7bf88ffef17524e0b9432281c1b72535
Souza-L-01 <souza.vilela.lucas@gmail.com>
2023-08-04 16:35:17 +0200

    funtion order

--- 8_Functions/funtion2.py
+++ 8_Functions/funtion2.py
@@ -1,2 +1,9 @@
 def say_hi():
-    print("Hello User")
+    print("Hello User") # This is a function, indentation is important
+
+print("Top")
+say_hi() # This is how you call a function
+print("Bottom")
+# Top
+# Hello User
+# Bottom
