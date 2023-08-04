865ada8cde9596f1a425b0f5393615afe546d11a
Souza-L-01 <souza.vilela.lucas@gmail.com>
2023-08-04 16:28:02 +0200

    list of tuples

--- 2_Lists/tuples.py
+++ 2_Lists/tuples.py
@@ -1,3 +1,6 @@
 coordinates = (4, 5) # Tuples are immutable
 # coordinates[1] = 10 # This will throw an error, cannot add value
 print(coordinates[0])
+
+# List of tuples
+coordinates = [(4, 5), (6, 7), (80, 34)]
