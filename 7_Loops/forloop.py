e6d031ea064feee2eb25a175f67072a6dbddd41d
Souza-L-01 <souza.vilela.lucas@gmail.com>
2023-08-06 15:41:15 +0200

    for loop in rage of interations

--- 7_Loops/forloop.py
+++ 7_Loops/forloop.py
@@ -10,3 +10,8 @@ for index in range(3, 10):
   print(index)
 for index in range(len(friends)):
   print(friends[index])
+for index in range(5):
+  if index == 0:
+    print("First Iteration")
+  else:
+    print("Not first")
