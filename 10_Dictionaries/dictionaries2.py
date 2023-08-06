0aafaae893cf7b75c6fcc1ad4ce4427d52ca3113
Souza-L-01 <souza.vilela.lucas@gmail.com>
2023-08-06 14:58:59 +0200

    strat dictionary

---
+++ 10_Dictionaries/dictionaries2.py
@@ -0,0 +1,16 @@
+monthConvertions = {
+  "Jan": "January",
+  "Feb": "February",
+  "Mar": "March",
+  "Apr": "April",
+  "May": "May",
+  "Jun": "June",
+  "Jul": "July", 
+  "Aug": "August"
+  "Sep": "Septemper"
+  "Oct": "October"
+  "Nov": "November"
+  "Dec": "December"
+}
+
+print(monthConvertions["Nov"])
