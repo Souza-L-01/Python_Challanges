87f27e75e62a1bbfa70a4ae0ad9759f8e1363574
Souza-L-01 <souza.vilela.lucas@gmail.com>
2023-08-06 14:51:20 +0200

    add calc structure

--- 3_Operators/calculator.py
+++ 3_Operators/calculator.py
@@ -1,4 +1,8 @@
-num1 = input("Enter a number: ")
-num2 = input("Enter another number: ")
-result = float(num1) + float(num2) #float converts string to number #int converts string to integer
-print(result)
+# num1 = input("Enter a number: ")
+# num2 = input("Enter another number: ")
+# result = float(num1) + float(num2) #float converts string to number #int converts string to integer
+# print(result)
+
+num1 = float(input("Entrer first number: "))
+op = input("Enter operator: ")
+num2  = float(input("Enter second number: "))
