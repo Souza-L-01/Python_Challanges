09df8f3710d01c74630a8a4800fe3969ce87b77b
Souza-L-01 <souza.vilela.lucas@gmail.com>
2023-08-06 15:37:50 +0200

    for index in rande

--- 7_Loops/forloop.py
+++ 7_Loops/forloop.py
@@ -5,3 +5,8 @@ fruits = ['banana', 'apple',  'mango']
 for fruit in fruits:        # Second Example
   print ('Current fruit :', fruit[0])
 
+frinds = ['Jim', 'Karen', 'Kevin']
+for index in range(3, 10):
+  print(index)
+for index in range(len(friends)):
+  print(friends[index])
