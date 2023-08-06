79dd055d7ecfd1a810fb48ca873e21bdff4cd75c
Souza-L-01 <souza.vilela.lucas@gmail.com>
2023-08-06 15:39:04 +0200

    for each interation friends array

--- 7_Loops/forloop.py
+++ 7_Loops/forloop.py
@@ -5,7 +5,7 @@ fruits = ['banana', 'apple',  'mango']
 for fruit in fruits:        # Second Example
   print ('Current fruit :', fruit[0])
 
-frinds = ['Jim', 'Karen', 'Kevin']
+friends = ['Jim', 'Karen', 'Kevin']
 for index in range(3, 10):
   print(index)
 for index in range(len(friends)):
