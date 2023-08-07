77422de19ecd1ff9508d1fe892a441fdf15366cd
Souza-L-01 <souza.vilela.lucas@gmail.com>
2023-08-07 19:29:40 +0200

    2d list/lists

--- 3_Operators/calculator.py
+++ 3_Operators/calculator.py
@@ -1,22 +1,22 @@
-# num1 = input("Enter a number: ")
-# num2 = input("Enter another number: ")
-# result = float(num1) + float(num2) #float converts string to number #int converts string to integer
-# print(result)
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+result = float(num1) + float(num2) #float converts string to number #int converts string to integer
+print(result)
 
-# num1 = float(input("Entrer first number: "))
-# op = input("Enter operator: ")
-# num2  = float(input("Enter second number: "))
+num1 = float(input("Entrer first number: "))
+op = input("Enter operator: ")
+num2  = float(input("Enter second number: "))
 
-# if op == "+":
-#   print(num1 + num2)
-# elif op == "-":
-#   print(num1 - num2)
-# elif  op == "/":
-#   print(num1 / num2)
-# elif op == "*":
-#   print(num1 * num2)
-# else:
-#   print("Invalid operator")
+if op == "+":
+  print(num1 + num2)
+elif op == "-":
+  print(num1 - num2)
+elif  op == "/":
+  print(num1 / num2)
+elif op == "*":
+  print(num1 * num2)
+else:
+  print("Invalid operator")
 
 # exponent 8_Functions
 
