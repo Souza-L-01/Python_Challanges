0645b83370d7c00260ea41afb3b510b20cc8365f
Souza-L-01 <souza.vilela.lucas@gmail.com>
2023-08-07 20:10:47 +0200

    statement inside statament

--- 8_Functions/translate.py
+++ 8_Functions/translate.py
@@ -1,8 +1,22 @@
+# def translate(phrase):
+#   translations = ""
+#   for letter in phrase:
+#     if letter in "AEIOUaeiou":
+#       translations = translations + "g"
+#     else:
+#       translations = translations + letter
+#   return translations
+
+# print(translate(input("Enter a phrase: ")))
+
 def translate(phrase):
   translations = ""
   for letter in phrase:
-    if letter in "AEIOUaeiou":
-      translations = translations + "g"
+    if letter.lower() in "aeiou":
+      if letter.isupper():
+        translations = translations + "G"
+      else:
+        translations = translations + "g"
     else:
       translations = translations + letter
   return translations
