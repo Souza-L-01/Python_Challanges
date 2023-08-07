5f78635c22bc6bc876d8d8c729c098fd06bac6a8
Souza-L-01 <souza.vilela.lucas@gmail.com>
2023-08-07 19:24:49 +0200

    add input to user

--- 3_Operators/calculator.py
+++ 3_Operators/calculator.py
@@ -3,26 +3,29 @@
 # result = float(num1) + float(num2) #float converts string to number #int converts string to integer
 # print(result)
 
-num1 = float(input("Entrer first number: "))
-op = input("Enter operator: ")
-num2  = float(input("Enter second number: "))
+# num1 = float(input("Entrer first number: "))
+# op = input("Enter operator: ")
+# num2  = float(input("Enter second number: "))
 
-if op == "+":
-  print(num1 + num2)
-elif op == "-":
-  print(num1 - num2)
-elif  op == "/":
-  print(num1 / num2)
-elif op == "*":
-  print(num1 * num2)
-else:
-  print("Invalid operator")
+# if op == "+":
+#   print(num1 + num2)
+# elif op == "-":
+#   print(num1 - num2)
+# elif  op == "/":
+#   print(num1 / num2)
+# elif op == "*":
+#   print(num1 * num2)
+# else:
+#   print("Invalid operator")
 
 # exponent 8_Functions
 
-base_num = float
+base_num = float(input("Enter base number: "))
+pow_num = float(input("Enter power number: "))
 def raise_to_power(base_num, pow_num):
   result = 1
   for index in range(pow_num):
     result = result * base_num 
   return result
+
+print(raise_to_power(base_num, pow_num))
