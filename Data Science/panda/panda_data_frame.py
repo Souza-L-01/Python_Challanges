bebcc324330c899ea3dbf96ce0c4f28efd2359dc
Souza-L-01 <souza.vilela.lucas@gmail.com>
2023-08-09 19:38:49 +0200

    start panda dataFrame

---
+++ Data Science/panda/panda_data_frame.py
@@ -0,0 +1,3 @@
+# Pandas DataFrames
+# Pandas is a high-level data manipulation tool developed by Wes McKinney. It is built on the Numpy package and its key data structure is called the DataFrame. DataFrames allow you to store and manipulate tabular data in rows of observations and columns of variables.
+# 
