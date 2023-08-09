dde784f6648f263a961bcbd2cc8d19a1a6ae6d47
Souza-L-01 <souza.vilela.lucas@gmail.com>
2023-08-09 19:41:04 +0200

    set dict

--- Data Science/panda/panda_data_frame.py
+++ Data Science/panda/panda_data_frame.py
@@ -1,3 +1,8 @@
 # Pandas DataFrames
 # Pandas is a high-level data manipulation tool developed by Wes McKinney. It is built on the Numpy package and its key data structure is called the DataFrame. DataFrames allow you to store and manipulate tabular data in rows of observations and columns of variables.
-# 
+
+dict = {"country": ["Brazil", "Russia", "India", "China", "South Africa"],
+       "capital": ["Brasilia", "Moscow", "New Dehli", "Beijing", "Pretoria"],
+       "area": [8.516, 17.10, 3.286, 9.597, 1.221],
+       "population": [200.4, 143.5, 1252, 1357, 52.98] }
+
