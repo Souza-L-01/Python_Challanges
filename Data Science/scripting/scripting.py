2c8b1fede83f809f67c79a75d73de4cdd0e10e64
Souza-L-01 <souza.vilela.lucas@gmail.com>
2023-08-10 17:22:06 +0200

    test read file

--- Data Science/scripting/scripting.py
+++ Data Science/scripting/scripting.py
@@ -1,4 +1,7 @@
-open("text_sample.txt", "r") # read
-open("text_sample.txt", "r+") # read and write
-open("text_sample.txt", "w") # write
-open("text_sample.txt", "a") # append
+names_file = open("text_sample.txt", "r") # read
+print(names_file.readable())
+names_file.close()
+# open("text_sample.txt", "r+") # read and write
+# open("text_sample.txt", "w") # write
+# open("text_sample.txt", "a") # append
+
