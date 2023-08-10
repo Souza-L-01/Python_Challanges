6a663089d0b4fe09e87ee354ab74ad0a00949489
Souza-L-01 <souza.vilela.lucas@gmail.com>
2023-08-10 17:35:53 +0200

    test printing

--- Data_science/scripting/scripting.py
+++ Data_science/scripting/scripting.py
@@ -1,6 +1,9 @@
 text_sample = open("text_sample.txt", "r") # read
 
-print(text_sample.readable())
+print(text_sample.read())
+# print(text_sample.readable())
+# print(text_sample.readline())
+# print(text_sample.readlines())
 
 text_sample.close()
 # open("text_sample.txt", "r+") # read and write
