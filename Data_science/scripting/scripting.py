02269244f5a7f015d44a9d230043a5a3de447cc1
Souza-L-01 <souza.vilela.lucas@gmail.com>
2023-08-10 17:50:35 +0200

    append

--- Data_science/scripting/scripting.py
+++ Data_science/scripting/scripting.py
@@ -1,5 +1,5 @@
 
-
+# mode: r, w, a, r+, w+, a+
 # open("text_sample.txt", "r+") # read and write
 # open("text_sample.txt", "w") # write
 # open("text_sample.txt", "a") # append
@@ -7,10 +7,14 @@
 # print(text_sample.read())
 # print(text_sample.readable())
 # print(text_sample.readline())
-text_sample = open("text_sample.txt", "r") # read
-for line in text_sample.readlines():
-  print(line)
+
+# text_sample = open("text_sample.txt", "r") # read
+# for line in text_sample.readlines():
+#   print(line)
 # print(text_sample.readlines()[1])
+# text_sample.close()
+
+ 
+text_sample = open("text_sample.txt", "a")
+text_sample.write("Toby - Human Resources")
 text_sample.close()
-
-
