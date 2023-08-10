2450d8b3706ff43496c45ad5aa90352757754020
Souza-L-01 <souza.vilela.lucas@gmail.com>
2023-08-10 17:06:05 +0200

    add specific error to 'except block'

--- 0_values/except_block.py
+++ 0_values/except_block.py
@@ -1,6 +1,9 @@
 # 1. Try block
 try:
+  answer = 10 / 0
   number = int(input("Enter a number: "))
   print(number)
+except ZeroDivisionError as err:
+  print("Divided by zero")
 except ValueError:
   print("Invalid input")
