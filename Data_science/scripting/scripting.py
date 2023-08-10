90c83f3f7cc992994c60f5084c63257b9dadf017
Souza-L-01 <souza.vilela.lucas@gmail.com>
2023-08-10 17:41:11 +0200

    for loop interation to each line

--- Data_science/scripting/scripting.py
+++ Data_science/scripting/scripting.py
@@ -1,12 +1,16 @@
-text_sample = open("text_sample.txt", "r") # read
 
-print(text_sample.read())
-# print(text_sample.readable())
-# print(text_sample.readline())
-# print(text_sample.readlines())
 
-text_sample.close()
 # open("text_sample.txt", "r+") # read and write
 # open("text_sample.txt", "w") # write
 # open("text_sample.txt", "a") # append
+
+# print(text_sample.read())
+# print(text_sample.readable())
+# print(text_sample.readline())
+text_sample = open("text_sample.txt", "r") # read
+for line in text_sample.readlines():
+  print(line)
+# print(text_sample.readlines()[1])
+text_sample.close()
+
 
