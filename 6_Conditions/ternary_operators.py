9e0dfd97471350706a84c148b519b1c4006e2af2
Souza-L-01 <souza.vilela.lucas@gmail.com>
2023-08-11 20:07:09 +0200

    print tests

--- 6_Conditions/ternary_operators.py
+++ 6_Conditions/ternary_operators.py
@@ -14,3 +14,20 @@ print just_the_buzz(10)
     print("Nope. It's Falsey.")
 
 
+# Empty container objects are considered Falsey.
+print truthy_test([])
+# Nope. It's Falsey.
+
+print truthy_test(['bear', 'pig', 'giraffe'])
+# This is Truthy.
+
+# Empty strings are considered Falsey.
+print truthy_test('')
+# Nope. It 's Falsey.
+
+print truthy_test('yes')
+# This is Truthy.
+
+# 0 is also considered Falsey.
+print truthy_test(0)
+# Nope. It's Falsey.
