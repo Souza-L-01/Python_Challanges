38f472d578d341ac957d90b304695de21ccd6f80
Souza-L-01 <souza.vilela.lucas@gmail.com>
2023-08-11 20:06:54 +0200

    try truthy_test

--- 6_Conditions/ternary_operators.py
+++ 6_Conditions/ternary_operators.py
@@ -1,8 +1,16 @@
 def just_the_buzz(number):
-    return 'Buzz!' if number % 5 == 0 else str(number)
+  return 'Buzz!' if number % 5 == 0 else str(number)
     
 print just_the_buzz(15)
 'Buzz!'
 
 print just_the_buzz(10)
 '10'
+
+>>> def truthy_test(thing):
+  if thing:
+    print('This is Truthy.')
+  else:
+    print("Nope. It's Falsey.")
+
+
